54e7e088ca2a44f817b4ab21eb6f22066c7cecc1
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-02-14 16:37:49 +0100

    Update totd.js

--- src/cmds/totd/totd.js
+++ src/cmds/totd/totd.js
@@ -100,9 +100,9 @@ module.exports = function(client, message, prefix, config, sql){
                     var monthsBack
                     var i = 0
                     var monthsShortNew
-                    monthsShort.reverse()
-                    for (var p = 0 ; p < new Date().getMonth() ; p++){
-                        monthsShortNew = arrayMove(monthsShort, 0, -1)
+                    var monthsReversed = monthsShort.reverse()
+                    for (var p = 0 ; p < monthsShort.indexOf(args[0].toLowerCase()) ; p++){
+                        monthsShortNew = arrayMove(monthsReversed, 0, -1)
                     }
                     console.log(monthsShortNew)
                     monthsShortNew.forEach(m=>{
