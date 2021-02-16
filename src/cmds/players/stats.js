f16d80b4b19c7a969935d37b3f5f24b521cb9373
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-02-16 23:32:27 +0100

    Update stats.js

--- src/cmds/players/stats.js
+++ src/cmds/players/stats.js
@@ -15,13 +15,16 @@ module.exports = function (client, message, prefix, config, sql){
                 } else {
                     players.player(res[0].accountId).then(player=>{
                         var trophies_str = []
+                        var totalTrophies = 0
                         player.trophies.counts.forEach(tier=>{
+                            totalTrophies = totalTrophies + tier
                             if (tier>0) trophies_str.push(`${trophies_str.length+1} : ${tier}`)
                         })
+                        trophies_str.push(`Total : ${totalTrophies}`)
                         let embed = new Discord.MessageEmbed()
                         .setTitle('Statictics of '+ player.displayname)
                         .setDescription(player.displayname + ' has started playing Trackmania on '+ new Date(player.timestamp).getFullYear()+'-'+(new Date(player.timestamp).getMonth()+1)+'-'+new Date(player.timestamp).getDate() +' (' + ms(new Date() - new Date(player.timestamp), {compact: true, verbose: true}) + ' ago).\nThis player was last seen ' + ms(new Date() - new Date(player.trophies.timestamp), {compact: true, verbose: true}) + ' ago.\nIts Trackmania.io URL is ' + player.url)
-                        .addField('Ranking:', `${player.trophies.points} points.\nNumber of trophies:\n${trophies_str.join('\n')}`, true)
+                        .addField('Ranking:', `${player.trophies.points} points.\n__Number of trophies__:\n${trophies_str.join('\n')}`, true)
                         .addField('Matchmaking:', `**__Teams 3v3:__**\nScore: ${player.matchmaking.find(m=>m.info.typename == '3v3').info.score} (${player.matchmaking.find(m=>m.info.typename == '3v3').info.rank.name})\nRank: ${player.matchmaking.find(m=>m.info.typename == '3v3').info.place}/${player.matchmaking.find(m=>m.info.typename == '3v3').total}`, true)
                         if (player.meta.nadeo || player.meta.tmgl || player.meta.team || player.meta.sponsor) embed.addField('Part of', `${player.meta.nadeo ? '- Nadeo Team\n' : ''}${player.meta.tmgl ? '- TMGL Player\n' : ''}${player.meta.team ? '- Trackmania.io Team\n' : ''}${player.meta.sponsor ? '- Trackmania.io Sponsor\n' : ''}`)
                         embed.setFooter(`Account id: ${player.accountid}`)
@@ -33,13 +36,16 @@ module.exports = function (client, message, prefix, config, sql){
         } else {
             players.player(args.join(' ')).then(player=>{
                 var trophies_str = []
+                var totalTrophies = 0
                 player.trophies.counts.forEach(tier=>{
+                    totalTrophies = totalTrophies + tier
                     if (tier>0) trophies_str.push(`${trophies_str.length+1} : ${tier}`)
                 })
+                trophies_str.push(`Total : ${totalTrophies}`)
                 let embed = new Discord.MessageEmbed()
                 .setTitle('Statictics of '+ player.displayname)
                 .setDescription(player.displayname + ' has started playing Trackmania on '+ new Date(player.timestamp).getFullYear()+'-'+(new Date(player.timestamp).getMonth()+1)+'-'+new Date(player.timestamp).getDate() +' (' + ms(new Date() - new Date(player.timestamp), {compact: true, verbose: true}) + ' ago).\nThis player was last seen ' + ms(new Date() - new Date(player.trophies.timestamp), {compact: true, verbose: true}) + ' ago.\nIts Trackmania.io URL is ' + player.url)
-                .addField('Ranking:', `${player.trophies.points} points.\nNumber of trophies:\n${trophies_str.join('\n')}`, true)
+                .addField('Ranking:', `${player.trophies.points} points.\n__Number of trophies__:\n${trophies_str.join('\n')}`, true)
                 .addField('Matchmaking:', `**__Teams 3v3:__**\nScore: ${player.matchmaking.find(m=>m.info.typename == '3v3').info.score} (${player.matchmaking.find(m=>m.info.typename == '3v3').info.rank.name})\nRank: ${player.matchmaking.find(m=>m.info.typename == '3v3').info.place}/${player.matchmaking.find(m=>m.info.typename == '3v3').total}`, true)
                 if (player.meta.nadeo || player.meta.tmgl || player.meta.team || player.meta.sponsor) embed.addField('Part of', `${player.meta.nadeo ? '- Nadeo Team\n' : ''}${player.meta.tmgl ? '- TMGL Player\n' : ''}${player.meta.team ? '- Trackmania.io Team\n' : ''}${player.meta.sponsor ? '- Trackmania.io Sponsor\n' : ''}`)
                 embed.setFooter(`Account id: ${player.accountid}`)
@@ -50,18 +56,21 @@ module.exports = function (client, message, prefix, config, sql){
                 players.searchPlayer(args.join(' ')).then(player=>{
                     players.player(player[0].accountid).then(player=>{
                         var trophies_str = []
-                player.trophies.counts.forEach(tier=>{
-                    if (tier>0) trophies_str.push(`${trophies_str.length+1} : ${tier}`)
-                })
-                let embed = new Discord.MessageEmbed()
-                .setTitle('Statictics of '+ player.displayname)
-                .setDescription(player.displayname + ' has started playing Trackmania on '+ new Date(player.timestamp).getFullYear()+'-'+(new Date(player.timestamp).getMonth()+1)+'-'+new Date(player.timestamp).getDate() +' (' + ms(new Date() - new Date(player.timestamp), {compact: true, verbose: true}) + ' ago).\nThis player was last seen ' + ms(new Date() - new Date(player.trophies.timestamp), {compact: true, verbose: true}) + ' ago.\nIts Trackmania.io URL is ' + player.url)
-                .addField('Ranking:', `${player.trophies.points} points.\nNumber of trophies:\n${trophies_str.join('\n')}`, true)
-                .addField('Matchmaking:', `**__Teams 3v3:__**\nScore: ${player.matchmaking.find(m=>m.info.typename == '3v3').info.score} (${player.matchmaking.find(m=>m.info.typename == '3v3').info.rank.name})\nRank: ${player.matchmaking.find(m=>m.info.typename == '3v3').info.place}/${player.matchmaking.find(m=>m.info.typename == '3v3').total}`, true)
-                if (player.meta.nadeo || player.meta.tmgl || player.meta.team || player.meta.sponsor) embed.addField('Part of', `${player.meta.nadeo ? '- Nadeo Team\n' : ''}${player.meta.tmgl ? '- TMGL Player\n' : ''}${player.meta.team ? '- Trackmania.io Team\n' : ''}${player.meta.sponsor ? '- Trackmania.io Sponsor\n' : ''}`)
-                embed.setFooter(`Account id: ${player.accountid}`)
-                if (player.meta.comment != "") embed.addField('Comment:', player.meta.comment)
-                message.channel.send(embed)
+                        var totalTrophies = 0
+                        player.trophies.counts.forEach(tier=>{
+                            totalTrophies = totalTrophies + tier
+                            if (tier>0) trophies_str.push(`${trophies_str.length+1} : ${tier}`)
+                        })
+                        trophies_str.push(`Total : ${totalTrophies}`)
+                        let embed = new Discord.MessageEmbed()
+                        .setTitle('Statictics of '+ player.displayname)
+                        .setDescription(player.displayname + ' has started playing Trackmania on '+ new Date(player.timestamp).getFullYear()+'-'+(new Date(player.timestamp).getMonth()+1)+'-'+new Date(player.timestamp).getDate() +' (' + ms(new Date() - new Date(player.timestamp), {compact: true, verbose: true}) + ' ago).\nThis player was last seen ' + ms(new Date() - new Date(player.trophies.timestamp), {compact: true, verbose: true}) + ' ago.\nIts Trackmania.io URL is ' + player.url)
+                        .addField('Ranking:', `${player.trophies.points} points.\n__Number of trophies__:\n${trophies_str.join('\n')}`, true)
+                        .addField('Matchmaking:', `**__Teams 3v3:__**\nScore: ${player.matchmaking.find(m=>m.info.typename == '3v3').info.score} (${player.matchmaking.find(m=>m.info.typename == '3v3').info.rank.name})\nRank: ${player.matchmaking.find(m=>m.info.typename == '3v3').info.place}/${player.matchmaking.find(m=>m.info.typename == '3v3').total}`, true)
+                        if (player.meta.nadeo || player.meta.tmgl || player.meta.team || player.meta.sponsor) embed.addField('Part of', `${player.meta.nadeo ? '- Nadeo Team\n' : ''}${player.meta.tmgl ? '- TMGL Player\n' : ''}${player.meta.team ? '- Trackmania.io Team\n' : ''}${player.meta.sponsor ? '- Trackmania.io Sponsor\n' : ''}`)
+                        embed.setFooter(`Account id: ${player.accountid}`)
+                        if (player.meta.comment != "") embed.addField('Comment:', player.meta.comment)
+                        message.channel.send(embed)
                     })
                     .catch(err=>{
                         console.error(err)
