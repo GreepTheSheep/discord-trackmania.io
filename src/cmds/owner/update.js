144a7355fb0736a5f11f06d86f662e093dda3880
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-02-19 23:16:11 +0100

    Update update.js

--- src/cmds/owner/update.js
+++ src/cmds/owner/update.js
@@ -6,10 +6,9 @@ module.exports = function(client, message, prefix) {
     if (message.content.startsWith(prefix + 'update')) {
         try {
             message.channel.startTyping()
-            if (client.user.id == '569624646475972608') shell.exec('git pull && npm update && pm2 reload ecosystem.config.js', {silent:false}, function(code, stdout, stderr) {
-                message.reply(`Output:\n\`\`\`${stdout}${stderr}\`\`\``).then(m=>message.channel.stopTyping(true));
+            shell.exec('git pull && npm update && pm2 reload ecosystem.config.js', {silent:false}, function(code, stdout, stderr) {
+                message.reply(`Output:\n\`\`\`${stdout}${stderr}\`\`\``).then(()=>message.channel.stopTyping(true));
             })
-            else return message.reply('Not supported yet, please execute commands `git pull && npm update && pm2 reload ecosystem.config.js` with !ssh command')
         } catch (err) {
             message.reply(`EVAL **__ERROR__**\n\`\`\`xl\n'update'\`\`\``);
             message.channel.stopTyping(true)
