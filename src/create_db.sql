387532985a7d491ea01c5d0cfee2bdf60e1192c1
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-03-04 11:00:44 +0100

    Create create_db.sql

---
+++ src/create_db.sql
@@ -0,0 +1,7 @@
+-- CREATE DATABASE IF NOT EXISTS 'trackmaniaio-bot' CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- GRANT ALL PRIVILEGES ON 'trackmaniaio-bot'.* TO 'trackmaniaio-bot'@'localhost' IDENTIFIED BY 'trackmaniaio';
+
+CREATE TABLE IF NOT EXISTS `totd_thumbnail_cache` ( `mapUid` VARCHAR(100) NOT NULL , `date` DATE NOT NULL , `thumbnail` TEXT NOT NULL , PRIMARY KEY (`mapUid`));
+CREATE TABLE IF NOT EXISTS `totd-wr_channels` ( `guildId` VARCHAR(64) NOT NULL , `userId` VARCHAR(64) NOT NULL , `channelId` VARCHAR(64) NOT NULL , PRIMARY KEY (`guildId`), UNIQUE (`channelId`));
+CREATE TABLE IF NOT EXISTS `totd_channels` ( `guildId` VARCHAR(64) NOT NULL , `userId` VARCHAR(64) NOT NULL , `channelId` VARCHAR(64) NOT NULL , PRIMARY KEY (`guildId`), UNIQUE (`channelId`));
+CREATE TABLE IF NOT EXISTS `news_channels` ( `guildId` VARCHAR(64) NOT NULL , `userId` VARCHAR(64) NOT NULL , `channelId` VARCHAR(64) NOT NULL , PRIMARY KEY (`guildId`), UNIQUE (`channelId`));
