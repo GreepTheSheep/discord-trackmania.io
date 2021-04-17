bb1fbb2f0539a8e1698ad0c5f0b4384609276093
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-04-17 20:09:56 +0200

    Added campaign leaderboard (waiting for getting global leaderboard in dependency)

---
+++ src/cmds/campaign/campaignLeaderboard.js
@@ -0,0 +1,65 @@
+// eslint-disable-next-line no-unused-vars
+const Discord = require('discord.js');
+const Trackmania = require('trackmania.io')
+const campaign = new Trackmania.Campaigns({listener: false})
+
+module.exports = function(client, message, prefix, config, sql) {
+    if (message.content.startsWith(prefix + 'campaignleaderboard') || message.content.startsWith(prefix + 'campaignleader')) {
+        let args = message.content.split(" ")
+        args.shift()
+        if (args.length < 1) return message.reply('Usage: `'+prefix+'campaignleader [Club ID] [Campaign ID] (optional: sub/unsub)`. Use sub to get any map WR updates on the campaign, unsub to unsubscribe.')
+        if (args.length < 2) return message.reply('Usage: `'+prefix+'campaignleader [Club ID] [Campaign ID] (optional: sub/unsub)`. We need a club ID because of Trackmania.io API. Use sub to get any map WR updates on the campaign, unsub to unsubscribe.')
+
+        var clubID = Number(args[0])
+        var campaignID = Number(args[1])
+
+        if (isNaN(clubID) || isNaN(campaignID)) return message.reply('Club ID or Campaign ID is not numbers. Check your syntax and retry')
+
+        if (clubID == 0) return message.reply('Official campaigns are not actually supported. An update will come later, just be patient ;)')
+
+        campaign.campaign(clubID, campaignID).then(thisCampaign=>{
+            if (thisCampaign.error) return message.reply('Invalid campaign or club ID, check the campaign\'s page at trackmania.io and copy paste the corresponding IDs.')
+            else {
+                args.shift()
+                if (args.length < 1){
+
+                    // return campaign leaderboard (waiting for the feature on the dependency)
+                    
+                } else if (args[0].toLowerCase() == 'sub'){
+                    if (!message.member.hasPermission('ADMINISTRATOR')) return message.reply(`:warning: Only administrators on this server can do that.`)
+                    var channel = message.mentions.channels.first()
+                    if (!channel) return message.reply(`Usage \`${prefix}campaignleader ${clubID} ${campaignID} sub [channel mention]\``)
+
+                    thisCampaign.playlist.forEach(map=>{
+                        sql.query('INSERT INTO `map-wr_channels` (userId, guildId, channelId, mapUid) VALUES (?, ?, ?, ?)', [message.author.id, message.guild.id, channel.id, map.mapUid], (err) =>{
+                            if (err){
+                                if (err.code == 'ER_DUP_ENTRY'){
+                                    sql.query("UPDATE `map-wr_channels` SET channelId = ? WHERE guildId = ? AND `mapUid` = ?", [channel.id, message.guild.id, map.mapUid], (err)=>{
+                                        if (err){
+                                            console.error(err)
+                                            client.users.cache.find(u => u.id == config.owner_id).send(`:warning: Error on campaing map WR sub event: \`\`\`${err}\`\`\``)
+                                        }
+                                    })
+                                } else {
+                                    client.users.cache.find(u => u.id == config.owner_id).send(`:warning: Error on campaign map WR sub event: \`\`\`${err}\`\`\``)
+                                }
+                            }
+                        })
+                        message.channel.send(`Successfully added #${channel.name} to get all maps World Record updates on the campaign ${thisCampaign.name}.`)
+                    })
+                } else if (args[0].toLowerCase().startsWith('unsub')){
+                    if (!message.member.hasPermission('ADMINISTRATOR')) return message.reply(`:warning: Only administrators on this server can do that.`)
+                    thisCampaign.playlist.forEach(map=>{
+                        sql.query('DELETE FROM `map-wr_channels` WHERE `guildId` = ? AND `mapUid` = ?', [message.guild.id, map.mapUid], (err) =>{
+                            if (err){
+                                console.error(err)
+                                client.users.cache.find(u => u.id == config.owner_id).send(`:warning: Error on map unsub event: \`\`\`${err}\`\`\``)
+                            }
+                        })
+                    })
+                    message.channel.send(`Successfully deleted all maps World Record updates for ${thisCampaign.name} on this server.`)
+                }
+            }
+        })
+    }
+}
