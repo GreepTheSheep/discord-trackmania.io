22a7f49b225b287a6cbc336c6c31d10691015c91
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-04-24 11:59:16 +0200

    Activate status (with some modifications)

--- src/events/activites.js
+++ src/events/activites.js
@@ -1,17 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 const Discord = require('discord.js')
-/*
 const ms = require('pretty-ms')
 const Trackmania = require('trackmania.io')
 
 function randomItem(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
-*/
 
 module.exports = function(client, config){
     client.user.setActivity(config.prefix+'help', {type: 'LISTENING'})
-    /*
     setInterval(()=>{
         const activities = [
             config.prefix+'help',
@@ -25,7 +22,7 @@ module.exports = function(client, config){
                 totd.reverse()
                 totd = totd[0]
                 if (status == 'totd name'){
-                    client.user.setActivity(`Today's TOTD is called ${totd.map.name} and it's created by ${totd.map.authordisplayname}`, {type: 'WATCHING'})
+                    client.user.setActivity(`Today's TOTD is called ${totd.map.name} and it's made by ${totd.map.authordisplayname}`, {type: 'WATCHING'})
                 } else if (status == 'totd wr'){
                     Trackmania.leaderboard(totd.map.mapUid).then(leader=>{
                         client.user.setActivity(`TOTD WR is at ${ms(leader[0].time, {colonNotation: true, secondsDecimalDigits: 3})} by ${leader[0].displayname}`, {type: 'WATCHING'})
@@ -34,5 +31,4 @@ module.exports = function(client, config){
             })
         } else client.user.setActivity(status, {type: 'LISTENING'})
     }, 2*60*1000)
-    */
 }
