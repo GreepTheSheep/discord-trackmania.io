2b949a0ab9aee6908a1d92b8c1c7d1a9b628b5ff
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-06-04 17:33:02 +0200

    Fix sql error

--- src/cmds/owner/setDefaultPrefixAllGuilds.js
+++ src/cmds/owner/setDefaultPrefixAllGuilds.js
@@ -10,23 +10,23 @@ module.exports = function (client, message, prefix, sql, config){
                 res.forEach(r => {
                     guildsInSQL.push(r.guildId)
                 });
-            }
-        })
 
-        var affectedGuilds = []
-        var errorGuilds = []
-        client.guilds.cache.forEach(guild=>{
-            if (!guildsInSQL.includes(guild.id)){
-                sql.query("INSERT INTO `prefix` (guildId, prefix) VALUES (?, ?, ?)", [guild.id, config.prefix], (err)=>{
-                    if (err){
-                        message.reply('Error for guild `'+guild.id+'`: ' + err)
-                        errorGuilds.push(guild.id)
+                var affectedGuilds = []
+                var errorGuilds = []
+                client.guilds.cache.forEach(guild=>{
+                    if (!guildsInSQL.includes(guild.id)){
+                        sql.query("INSERT INTO `prefix` (guildId, prefix) VALUES (?, ?)", [guild.id, config.prefix], (err)=>{
+                            if (err){
+                                message.reply('Error for guild `'+guild.id+'`: ' + err)
+                                errorGuilds.push(guild.id)
+                            }
+                            else affectedGuilds.push(guild.id)
+                        })
+                        
+                        message.reply(`Done. ${affectedGuilds.length} affected guilds: \`\`\`${affectedGuilds.join('\n')}\`\`\`${errorGuilds.length} guilds with error`)
                     }
-                    else affectedGuilds.push(guild.id)
                 })
             }
         })
-
-        message.reply(`Done. ${affectedGuilds.length} affected guilds: \`\`\`${affectedGuilds.join('\n')}\`\`\`${errorGuilds.length} guilds with error`)
     }
 }
