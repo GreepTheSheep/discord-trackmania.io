20d2f9d2afceda7ea54cbb0500c915c0ccd117b4
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-06-20 14:13:07 +0200

    Update club.js

--- src/cmds/clubs/club.js
+++ src/cmds/clubs/club.js
@@ -1,6 +1,19 @@
 const Discord = require('discord.js')
 const Trackmania = require('trackmania.io')
 
+function buildEmbed(club){
+    let embed = new Discord.MessageEmbed()
+    embed.setTitle(club.name)
+    .setDescription(club.description)
+    .setThumbnail(club.logoUrl)
+    .setImage(club.backgroundUrl)
+    .addField('Created by:', club.creatordisplayname, true)
+    .addField('Members', club.membercount, true)
+    embed.setFooter(`Club ID: ${club.id}`)
+
+    return embed
+}
+
 module.exports = function (client, message, prefix){
     if (message.content.toLowerCase().startsWith(prefix + 'club')){
         let args = message.content.split(" ").slice(1)
@@ -8,41 +21,13 @@ module.exports = function (client, message, prefix){
         if (args.length < 1) return message.reply(`Usage \`${prefix}club [Club name or Club ID]\``)
 
         clubs.club(args.join(' ')).then(club=>{
-            let embed = new Discord.MessageEmbed()
-            embed.setTitle(club.name)
-            .setDescription(club.description)
-            .setThumbnail(club.logoUrl)
-            .setImage(club.backgroundUrl)
-            .addField('Created by:', club.creatordisplayname, true)
-            .addField('Members', club.membercount, true)
-            var arrayMembers = []
-            club.members.forEach(member => {
-                arrayMembers.push(`${member.name} (${member.role})${member.vip && member.role != 'Creator' ? ' [VIP]' : ''}`)
-            });
-            embed.addField('Management:', '- ' + arrayMembers.join('\n- '))
-            embed.setFooter(`Club ID: ${club.id}`)
-
-            message.channel.send(embed)
+            message.channel.send(buildEmbed(club))
         })
         .catch(err=>{
             console.log(err)
             clubs.searchClubs(args.join(' ')).then(club=>{
                 clubs.club(club[0].id).then(club=>{
-                    let embed = new Discord.MessageEmbed()
-                    embed.setTitle(club.name)
-                    .setDescription(club.description)
-                    .setThumbnail(club.logoUrl)
-                    .setImage(club.backgroundUrl)
-                    .addField('Created by:', club.creatordisplayname, true)
-                    .addField('Members', club.membercount, true)
-                    var arrayMembers = []
-                    club.members.forEach(member => {
-                        arrayMembers.push(`${member.name} (${member.role})${member.vip && member.role != 'Creator' ? ' [VIP]' : ''}`)
-                    });
-                    embed.addField('Management:', '- ' + arrayMembers.join('\n- '))
-                    embed.setFooter(`Club ID: ${club.id}`)
-
-                    message.channel.send(embed)
+                    message.channel.send(buildEmbed(club))
                 })
                 .catch(err=>{
                     console.error(err)
