d8ea794e76bef7c3b5df20ba8d18f45a2e0a9e05
Matthieu Dehon <42576124+GreepTheSheep@users.noreply.github.com>
2021-12-01 21:53:03 +0100

    Add TOTD info on message

--- src/commands/totd/totdInfo.js
+++ src/commands/totd/totdInfo.js
@@ -1,6 +1,7 @@
 const Command = require('../../structures/Command'),
     FormData = require('form-data'),
     fetch = require('node-fetch'),
+    {getMapThumbnailEmbed} = require("../../utils"),
     {MessageEmbed, MessageActionRow, MessageButton, CommandInteraction, SelectMenuInteraction, Message, MessageAttachment} = require('discord.js'),
     MySQL = require('mysql'),
     ms = require('pretty-ms');
@@ -71,18 +72,17 @@ exports.execute = async (interaction, tmio, commands, sql) => {
         author = await map.author();
 
     embed.setColor('GREEN').setAuthor(`Track of The Day - ${date.getDate()} ${monthsArray[date.getMonth()]} ${date.getFullYear()}`)
-        .setTitle(tmio.formatTMText(map.name));
-
-    embed.addField('Created by:', author.name, true)
-    embed.addField('Medals:', `Author: **${ms(map.medalTimes.author, {colonNotation: true, secondsDecimalDigits: 3})}**\nGold: ${ms(map.medalTimes.gold, {colonNotation: true, secondsDecimalDigits: 3})}\nSilver: ${ms(map.medalTimes.silver, {colonNotation: true, secondsDecimalDigits: 3})}\nBronze: ${ms(map.medalTimes.bronze, {colonNotation: true, secondsDecimalDigits: 3})}`)
-    embed.addField('Uploaded:', `<t:${map.uploaded.getTime() / 1000}:R>`, true)
-    embed.setFooter(`Map UID: ${map.uid}`);
+        .setTitle(tmio.formatTMText(map.name))
+        .addField('Created by:', author.name, true)
+        .addField('Medals:', `Author: **${ms(map.medalTimes.author, {colonNotation: true, secondsDecimalDigits: 3})}**\nGold: ${ms(map.medalTimes.gold, {colonNotation: true, secondsDecimalDigits: 3})}\nSilver: ${ms(map.medalTimes.silver, {colonNotation: true, secondsDecimalDigits: 3})}\nBronze: ${ms(map.medalTimes.bronze, {colonNotation: true, secondsDecimalDigits: 3})}`)
+        .addField('Uploaded:', `<t:${map.uploaded.getTime() / 1000}:R>`, true)
+        .setFooter(`Map UID: ${map.uid}`);
 
 
     if (process.env.IMGUR_CLIENT_ID){
         // download map thumbnail and send it to Discord (because Nadeo Services can't serve thumbnails directly, so we need to dl)
         try{
-            embed.setImage(await require('../../utils').getMapThumbnailEmbed(tmio, map, sql));
+            embed.setImage(await getMapThumbnailEmbed(tmio, map, sql));
         } catch (err) {
             console.error(err);
         }
@@ -106,10 +106,10 @@ exports.execute = async (interaction, tmio, commands, sql) => {
             .setLabel('Trackmania.io')
             .setStyle('LINK')
         );
-    if (map.exchange) {
+    if (map.exchangeId) {
         interactionComponentRows[0].addComponents(
             new MessageButton()
-                .setURL(`https://trackmania.exchange/tracks/view/${map.exchange.id}`)
+                .setURL(`https://trackmania.exchange/tracks/view/${map.exchangeId}`)
                 .setLabel('Trackmania.exchange')
                 .setStyle('LINK')
             );
@@ -130,7 +130,72 @@ exports.execute = async (interaction, tmio, commands, sql) => {
  * @param {MySQL.Connection} sql
  */
 exports.executeMessage = async (message, args, tmio, commands, sql) => {
-    message.reply('coming soon');
+    const month = Number(args[1]),
+        day = Number(args[0]),
+        year = Number(args[2]),
+        embed = new MessageEmbed(),
+        monthsArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+        date = new Date();
+
+    if (month && day){
+        date = new Date(date.setMonth(month-1));
+        date = new Date(date.setDate(day));
+        if (year) date = new Date(date.setFullYear(year));
+    }
+
+    const totd = await tmio.totd.get(date),
+        map = await totd.map(),
+        author = await map.author();
+
+    embed.setColor('GREEN').setAuthor(`Track of The Day - ${date.getDate()} ${monthsArray[date.getMonth()]} ${date.getFullYear()}`)
+        .setTitle(tmio.formatTMText(map.name))
+        .addField('Created by:', author.name, true)
+        .addField('Medals:', `Author: **${ms(map.medalTimes.author, {colonNotation: true, secondsDecimalDigits: 3})}**\nGold: ${ms(map.medalTimes.gold, {colonNotation: true, secondsDecimalDigits: 3})}\nSilver: ${ms(map.medalTimes.silver, {colonNotation: true, secondsDecimalDigits: 3})}\nBronze: ${ms(map.medalTimes.bronze, {colonNotation: true, secondsDecimalDigits: 3})}`)
+        .addField('Uploaded:', `<t:${map.uploaded.getTime() / 1000}:R>`, true)
+        .setFooter(`Map UID: ${map.uid}`);
+
+
+    if (process.env.IMGUR_CLIENT_ID){
+        // download map thumbnail and send it to Discord (because Nadeo Services can't serve thumbnails directly, so we need to dl)
+        try{
+            embed.setImage(await getMapThumbnailEmbed(tmio, map, sql));
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    // create 2 interaction rows (button or select menus)
+    const interactionComponentRows = [];
+    for (let i = 0; i < 1; i++) {
+        interactionComponentRows.push(new MessageActionRow());
+    }
+
+    interactionComponentRows[0].addComponents(
+        new MessageButton()
+            .setURL(map.url)
+            .setLabel('Download Map')
+            .setStyle('LINK')
+        );
+    interactionComponentRows[0].addComponents(
+        new MessageButton()
+            .setURL(`https://trackmania.io/#/totd/leaderboard/${totd.leaderboardId}/${map.uid}`)
+            .setLabel('Trackmania.io')
+            .setStyle('LINK')
+        );
+    if (map.exchangeId) {
+        interactionComponentRows[0].addComponents(
+            new MessageButton()
+                .setURL(`https://trackmania.exchange/tracks/view/${map.exchangeId}`)
+                .setLabel('Trackmania.exchange')
+                .setStyle('LINK')
+            );
+    }
+
+    message.reply({
+        content: month && day ? '' : `Tip: You can set a specific TOTD day with \`/${this.name} [date-of-the-month] [month] <year>\`. Example \`/${this.name} 12 7\` to get the TOTD of July, 12, ${new Date().getFullYear()}`,
+        embeds: [embed],
+        components: interactionComponentRows
+    });
 };
 
 /**
