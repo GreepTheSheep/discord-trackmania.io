27594e2112346b2a2e5b98910cde6cab51e14f6e
Matthieu Dehon <42576124+GreepTheSheep@users.noreply.github.com>
2022-04-05 19:51:56 +0200

    Update pm2.config.js

--- pm2.config.js
+++ pm2.config.js
@@ -1,13 +1,12 @@
 module.exports = {
     apps : [{
       name: 'Trackmania.io Discord',
-      script: require('./data/config.json').dev.enable ? 'src/index.js' : 'shard.js',
+      script: 'src/index.js',
 
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
       autorestart: true,
-      watch: true,
-      ignore_watch : ["node_modules", "data", "logs", ".git"],
       max_memory_restart: '500M',
-      error_file: 'logs/errors.log'
+      out_file: 'logs/out.log',
+      error_file: 'logs/errors.log',
     }]
   };
