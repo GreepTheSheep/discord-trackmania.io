62ea9e32ad947e2195e608ada6d0050f92441108
Matthieu Dehon <42576124+GreepTheSheep@users.noreply.github.com>
2022-04-06 14:31:18 +0200

    deploy test

--- pm2.config.js
+++ pm2.config.js
@@ -1,12 +1,27 @@
+require('dotenv').config();
+var package = require('./package.json');
+
 module.exports = {
     apps : [{
-      name: 'Trackmania.io Discord',
-      script: 'src/index.js',
+        name: 'Trackmania.io Discord',
+        script: 'src/index.js',
 
-      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-      autorestart: true,
-      max_memory_restart: '500M',
-      out_file: 'logs/out.log',
-      error_file: 'logs/errors.log',
-    }]
-  };
+        // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+        autorestart: true,
+        max_memory_restart: '500M',
+        out_file: 'logs/out.log',
+        error_file: 'logs/errors.log',
+    }],
+
+    deploy: {
+        production: {
+            user: process.env.SERVER_USER,
+            host: process.env.SERVER_IP,
+            key: 'deploy.key',
+            ref: 'origin/main',
+            repo: package.repository.url,
+            path: process.env.SERVER_PATH,
+            'post-deploy': 'npm i && pm2 reload pm2.config.js --env production && pm2 save'
+        }
+    }
+};
